fc77339c3b419ecf2c1ead8d218126b683279542
Sean Roche <sean.roche@gmail.com>
2012-12-26 17:30:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenDocket::Application.config.secret_token = '64506f6aea8cb6e755a5878f3e327e7b15bcd6ae07a7bdd1a4c0e5579742f46656cd01f35a14a444b1f553360ade3b1f9089f0b0d0597d34e2fdb4dac6c5bab6'
