d01f27906a337b4603291b4b22ce0fd625e84c75
Sean Roche <sean.roche@gmail.com>
2012-12-30 08:14:53 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 OpenDocket::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Sean Roche"
+    email    "sean@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
+
